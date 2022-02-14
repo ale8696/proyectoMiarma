5a7616cc7ddc7412645e326672ef3e375d96f008
Alejandro Bajo <alebajodiaz@gmail.com>
2022-02-14 10:14:41 +0100

    first commit

---
+++ src/main/java/com/salesianos/dam/proyectoMiarma/ProyectoMiarmaApplication.java
@@ -0,0 +1,13 @@
+package com.salesianos.dam.proyectoMiarma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoMiarmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoMiarmaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salesianos/dam/proyectoMiarma/ProyectoMiarmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianos.dam.proyectoMiarma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoMiarmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
